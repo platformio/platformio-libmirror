4af50fc49f4c1cc9f5304c18d07ef23a089e7225
gandy92 <gandy92@googlemail.com>
2015-03-25 23:58:21 +0100

    Fix bug in determining number of registered libraries

--- scripts/github-top/mk-list-platformio.sh
+++ scripts/github-top/mk-list-platformio.sh
@@ -7,6 +7,7 @@ total=$(curl -s http://api.platformio.org/lib/search|sed -e s/,/\\n/g|grep total
 for ((i=1; $i <= $total; i++)); do
 	echo -en "\r$i/$total: " >&2
 	confurl=$(curl -s http://api.platformio.org/lib/info/$i|sed -e s/,/,\\n/g|grep confurl|cut -d\" -f4)
+	[ -z "$confurl" ] && total=$(expr $total + 1)
 	# curl -s "$confurl" > .json/$i
 	repo=$(curl -s "$confurl" |while read line; do echo -n $line|sed -e s/\\n//; done| sed -e s/\}/\\n/g|grep repository|sed -e s/,/\\n/g|grep url|cut -d\" -f4)
 	[ -n "$repo" ] && echo $i: $(echo $repo|sed -e s/.git$//)
