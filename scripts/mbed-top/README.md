b1b5a65f11987f1b4a464bfd9e0d82cf7dd141a6
gandy92 <gandy92@googlemail.com>
2015-04-10 17:45:55 +0200

    Improve mbed library item pipeline

--- scripts/mbed-top/README.md
+++ scripts/mbed-top/README.md
@@ -3,3 +3,9 @@
 `scrapy crawl mbedlib`
 
 downloads a few project pages and tries to produce manifest files
+
+To create and review data, use the following bash commands:
+```
+scrapy crawl mbedlib -o libs.jsonlines
+cat libs.jsonlines | while read line; do echo "$line"|json_pp -f json -t json -json_opt pretty,utf8; done
+```
--- scripts/mbed-top/mbedtop/pipelines.py
+++ scripts/mbed-top/mbedtop/pipelines.py
@@ -5,6 +5,7 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+import types
 
 class RepoPostProc(object):
     def process_item(self, item, spider):
@@ -15,14 +16,53 @@ class RepoPostProc(object):
         self.transfer_if(item,'deplist2','Dependencies:','dependencies')
         self.transfer_if(item,'deplist1','Dependents:','examples')
         self.transfer_if(item,'deplist2','Dependents:','examples')
-        # sometimes there is a link to subpage instead:
+        # sometimes there is a link to subpage instead of title "Dependencies":
         if ('repository' in item):
             rep = item['repository']
             if type(rep) is list: rep = rep[0]
             self.transfer_if(item,'deplist2',rep+'dependents','examples')
+
+        # prettify examples:
+        if ('examples' in item):
+            item['examples'] = self.make_mbed_url(item['examples'])
+
+        if ('repository' in item):
+            repo = { "type" : "hg", "url" : self.make_mbed_url(item['repository']) }
+            item['repository'] = repo
+
+        if ('components' in item):
+            authors = []
+            keywords = []
+            components = self.strip_mbed_url(item['components'])
+            #if not type(components) is list: components = [ components ]
+            for value in components:
+                if '/teams/' in value: authors.append(value)
+                if '/users/' in value: authors.append(value)
+                if '/components/' in value: keywords.append(value.replace('/components/', '').replace('/', ''))
+            #if len(authors): item['authors'] = authors
+            if len(keywords): item['keywords'] = keywords
+            del item['components']
+
         # todo: check components if contents need to go to authors or keywords
 
         return item
+
+    def strip_mbed_url(self, resource):
+        if isinstance(resource, types.StringTypes):
+            resource = resource.replace("https://developer.mbed.org", "")
+        elif type(resource) is list:
+            for i, value in enumerate(resource):
+                resource[i]= self.strip_mbed_url(value)
+        return resource
+
+    def make_mbed_url(self, resource):
+        if isinstance(resource, types.StringTypes):
+            if resource[0] == '/': 
+                resource = "https://developer.mbed.org"+resource
+        elif type(resource) is list:
+            for i, value in enumerate(resource):
+                resource[i]= self.make_mbed_url(value)
+        return resource
 
     # transfer list in key1 to key2 (except first element) 
     # if first element equals firstval
