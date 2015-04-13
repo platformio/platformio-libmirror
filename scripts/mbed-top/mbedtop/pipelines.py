4c48c060f018eb9be248735927e7675ddc88c115
gandy92 <gandy92@googlemail.com>
2015-04-13 12:54:31 +0200

    Make multipage extraction work and clean up code

--- scripts/mbed-top/mbedtop/pipelines.py
+++ scripts/mbed-top/mbedtop/pipelines.py
@@ -9,19 +9,6 @@ import types
 
 class RepoPostProc(object):
     def process_item(self, item, spider):
-        #
-        # transfer deplist1 and deplist2 to appropriate fields:
-        #
-	self.transfer_if(item,'deplist1','Dependencies:','dependencies')
-        self.transfer_if(item,'deplist2','Dependencies:','dependencies')
-        self.transfer_if(item,'deplist1','Dependents:','examples')
-        self.transfer_if(item,'deplist2','Dependents:','examples')
-        # sometimes there is a link to subpage instead of title "Dependencies":
-        if ('repository' in item):
-            rep = item['repository']
-            if type(rep) is list: rep = rep[0]
-            self.transfer_if(item,'deplist2',rep+'dependents','examples')
-
         # prettify examples:
         if ('examples' in item):
             item['examples'] = self.make_mbed_url(item['examples'])
@@ -64,16 +51,3 @@ class RepoPostProc(object):
                 resource[i]= self.make_mbed_url(value)
         return resource
 
-    # transfer list in key1 to key2 (except first element) 
-    # if first element equals firstval
-    def transfer_if(self, item, key1, firstval, key2):
-        if key1 in item.keys():
-            if item[key1][0] == firstval:
-                tmplist = item[key1]
-                del tmplist[0]
-                newlist = []
-                if key2 in item:
-                    newlist= item[key2]
-                newlist = tmplist
-                item[key2] = newlist
-                del item[key1]
--- scripts/mbed-top/mbedtop/spiders/mbedlib_spider.py
+++ scripts/mbed-top/mbedtop/spiders/mbedlib_spider.py
@@ -7,9 +7,10 @@ class MbedLibSpider(scrapy.Spider):
     name = "mbedlib"
     allowed_domains = ["developer.mbed.org"]
     start_urls = [
-        "http://developer.mbed.org/users/simon/code/TextLCD",
-        "http://developer.mbed.org/users/simon/code/TextLCD_HelloWorld/",
-        "http://developer.mbed.org/users/mbed_official/code/mbed/"
+        #"http://developer.mbed.org/users/simon/code/TextLCD",
+        #"http://developer.mbed.org/users/simon/code/TextLCD_HelloWorld/",
+        #"http://developer.mbed.org/users/mbed_official/code/mbed/",
+        "http://developer.mbed.org/users/mbed_official/code/EthernetInterface/"
     ]
 
     # parse the library page
@@ -23,48 +24,40 @@ class MbedLibSpider(scrapy.Spider):
         l.add_value('frameworks', 'mbed')
         l.add_value('platforms', ['freescalekinetis', 'nordicnrf51', 'nxplpc', 'ststm32'])
         l.add_xpath('components', '/html/body/div[4]/div[2]/div[3]//a/@href')
-        #TODO: clean up when multipage extraction works:
-        #l.add_xpath('deplist1', './/*[@id="mbed-content"]/p[2]/b/a/text()')
-        #l.add_xpath('deplist1', './/*[@id="mbed-content"]/p[2]/a/@href')
-        #l.add_xpath('deplist2', './/*[@id="mbed-content"]/p[3]/b/a/@href')
-        #l.add_xpath('deplist2', './/*[@id="mbed-content"]/p[3]/a/@href')
-        #l.add
-        #l.add_value('examples', [''])
-        #l.add_value('dependencies', [''])
         item = l.load_item()
 
         request = scrapy.Request(response.url+"dependencies",callback=self.parse_dependencies)
         request.meta['libpage'] = response.url
         request.meta['item'] = item
         return request
-        #TODO: clean up when multipage extraction works:
-        #return l.load_item()
 
     def parse_dependencies(self, response):
         item = response.meta['item']
         l = MbedLibLoader(item=item, response=response)
-        #TODO: formulate xpath to extract all links to dependencies
-        l.add_xpath('dependencies', '/html/body/div[4]/div[1]/div/a[2]/@href')
+        l.add_xpath('dependencies', './/*[@id="mbed-content"]//div/div[2]/div[2]/div[1]/b/a/@href')
         item = l.load_item()
         #TODO: generate requests for all dependents; ideally emit them before proceeding with examples
+        #if 'dependencies' in item:
+        #    for url in item['dependencies']: 
+        #        yield scrapy.Request(url,callback=self.parse)
 
         request = scrapy.Request(response.meta['libpage']+"dependents",callback=self.parse_examples)
         request.meta['libpage'] = response.meta['libpage']
         request.meta['item'] = item
-        return request
+        yield request
 
     def parse_examples(self, response):
         item = response.meta['item']
         l = MbedLibLoader(item=item, response=response)
-        #TODO: formulate xpath to extract all links to dependents (examples)
-        l.add_xpath('examples', '/html/body/div[4]/div[1]/div/a[2]/@href')
+        l.add_xpath('examples', './/*[@id="mbed-content"]//div/div[2]/div[2]/div[1]/b/a/@href')
         item = l.load_item()
         return item
+
         # we could also add a search for this specific library to fill in keywords from tags:
-        request = scrapy.Request(response.meta['libpage']+"",callback=self.parse_tags)
-        request.meta['libpage'] = response.meta['libpage']
-        request.meta['item'] = item
-        return request
+        #request = scrapy.Request(response.meta['libpage']+"",callback=self.parse_tags)
+        #request.meta['libpage'] = response.meta['libpage']
+        #request.meta['item'] = item
+        #return request
 
     def parse_tags(self, response):
         item = response.meta['item']
