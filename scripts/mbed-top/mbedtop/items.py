8925e54491408c2a3cae58ece654a844800f1fa9
Namo Aton <namoaton@gmail.com>
2018-03-02 21:53:26 +0200

    Mbed revalidate (#124)
    * mbed-top revalidate
    
    * fix

--- scripts/mbed-top/mbedtop/items.py
+++ scripts/mbed-top/mbedtop/items.py
@@ -3,26 +3,22 @@
 # Define here the models for your scraped items
 #
 # See documentation in:
-# http://doc.scrapy.org/en/latest/topics/items.html
+# https://doc.scrapy.org/en/latest/topics/items.html
 
 import scrapy
-import re
-
 from scrapy.loader import ItemLoader
 from scrapy.loader.processors import MapCompose, Join, TakeFirst, Compose
-
-# from w3lib.html import remove_entities
-
-#
-# for data distributed over more than one webpage:
-#
-# http://doc.scrapy.org/en/0.24/topics/request-response.html#topics-request-response-ref-request-callback-arguments
+import re
 
 
 def strip_component(str):
     if str:
         strlist = str.strip().split('\n')
-        if len(strlist):
+        str = ''
+        if len(strlist) > 1:
+            for s in strlist:
+                str = str + s
+        else:
             str = strlist[0]
         str = re.sub(r"\s+", " ", str.replace("\n", " ")).strip()
         return str
@@ -43,45 +39,21 @@ class CleanupList(object):
         return nvalues
 
 
-class MbedLibLoader(ItemLoader):
+class MbedParserLoader(ItemLoader):
     default_input_processor = MapCompose(strip_component)
     default_output_processor = CleanupList()
 
 
-class MbedLibItem(scrapy.Item):
+class MbedParserItem(scrapy.Item):
     # define the fields for your item here like:
-    repo_type = scrapy.Field()  # Program or Library
-    name = scrapy.Field()
-    authors = scrapy.Field()
-    description = scrapy.Field()
-    keywords = scrapy.Field()
-    owner = scrapy.Field()
-    ownerurl = scrapy.Field()
-    repository = scrapy.Field()
-    deplist1 = scrapy.Field()
-    deplist2 = scrapy.Field()
-    components = scrapy.Field()
-    dependencies = scrapy.Field()
-    examples = scrapy.Field()
-    frameworks = scrapy.Field()
-    platforms = scrapy.Field()
-    dependents = scrapy.Field()
-    fork = scrapy.Field()
+    # name = scrapy.Field()
     commits = scrapy.Field()
     imports = scrapy.Field()
-    pass
-
-
-class MbedItem(scrapy.Item):
-    # define the fields for your item here like:
-    name = scrapy.Field(output_processor=Compose(lambda v: v[0]))
+    author_name = scrapy.Field()
+    author_url = scrapy.Field()
+    name = scrapy.Field()
     description = scrapy.Field()
-    keywords = scrapy.Field(
-    )  # use components (link-elements rather than text)
-    authors = scrapy.Field()
-    repository = scrapy.Field()
-    frameworks = scrapy.Field()
-    platforms = scrapy.Field()
-    dependencies = scrapy.Field()  # get from %libpage%/dependencies
-    examples = scrapy.Field()  # get from %libpage%/dependents
-    pass
+    repo_url = scrapy.Field()
+    keywords = scrapy.Field()
+    examples = scrapy.Field()
+    dependencies = scrapy.Field()
