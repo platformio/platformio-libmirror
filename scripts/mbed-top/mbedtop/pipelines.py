0ec4e0dbb313eae2c449fd575dc85778c0edc3d8
Shahrustam <shahrustam98@gmail.com>
2020-01-06 19:42:42 +0200

    Update mbed libs. Minor fixes.

--- scripts/mbed-top/mbedtop/pipelines.py
+++ scripts/mbed-top/mbedtop/pipelines.py
@@ -6,39 +6,46 @@
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 import logging
 import json
+import hashlib
 import codecs
 import os
 logging.basicConfig()
 logger = logging.getLogger('mbed-pipeline-terrier')
-logger.setLevel(20)
+# logger.setLevel("WARNING")
 
 
 class MbedParserPipeline(object):
-    def print_item(self, lib_info):
-        print(json.dumps(
-            lib_info,
-            indent=4,
-            sort_keys=True,
-            separators=(',', ': '),
-            ensure_ascii=False))
+
+    def md5_file(self, fname):
+        with open(fname, "rb") as f:
+            hash_md5 = hashlib.md5(f.read())
+        return hash_md5.hexdigest()
 
     def write_to_json_file(self, json_item):
         path = os.path.dirname(os.path.realpath(__file__))
         repo_tokens = json_item['repository']['url'].split('/')
-        with codecs.open(
-                path + '/../../../configs/mbed/' + repo_tokens[6] + "_" +
-                repo_tokens[4] + ".json",
-                "w",
-                encoding='utf-8') as f:
-            logger.info("Save to %s_%s.json  " % (repo_tokens[6],
-                                                  repo_tokens[4]))
-            json.dump(
+        full_path = path + '/../../../configs/mbed/' + repo_tokens[6] + "_" + repo_tokens[4] + ".json"
+        dumped_json = json.dumps(
                 dict(json_item),
-                f,
                 indent=4,
                 sort_keys=True,
                 separators=(',', ': '),
                 ensure_ascii=False)
+        if os.path.isfile(full_path):
+            md5_file = self.md5_file(full_path)
+            md5_json = hashlib.md5(dumped_json.encode("utf-8")).hexdigest()
+            if md5_file == md5_json:
+                logger.warning("%s_%s.json already exist" % (repo_tokens[6],
+                                                     repo_tokens[4]))
+                return True
+        with open(
+                full_path,
+                "w",
+                encoding='utf-8') as f:
+            logger.warning("Save to %s_%s.json  " % (repo_tokens[6],
+                                                     repo_tokens[4]))
+            f.write(dumped_json)
+
 
     def process_item(self, item, spider):
         json_item = {
@@ -66,5 +73,4 @@ class MbedParserPipeline(object):
             json_item['keywords'] = item['keywords']
         else:
             json_item['keywords'] = json_item['name']
-        self.print_item(json_item)
         self.write_to_json_file(json_item)
